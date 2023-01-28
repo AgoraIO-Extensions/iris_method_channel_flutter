4f75e075e3421ebee17b8fecb20a25b9d94f751c
fenglang <fenglang@littlegnal-mbp13.local>
2023-01-28 17:00:46 +0800

    Introduce `NativeBindingDelegateProvider` to allow pass custom `NativeBindingDelegate` from outside.

--- lib/iris_method_channel.dart
+++ lib/iris_method_channel.dart
@@ -1,3 +1,5 @@
 export 'src/iris_method_channel.dart';
 export 'src/scoped_objects.dart';
 export 'src/iris_event.dart';
+export 'src/native_bindings_delegate.dart';
+export 'src/bindings/native_iris_api_common_bindings.dart';
