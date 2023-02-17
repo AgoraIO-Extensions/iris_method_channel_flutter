f26cdba2bfebcce9f2ed5ce25468aa6427ea400f
littleGnAl <tearjeaker@hotmail.com>
2023-02-17 16:11:58 +0800

    fix: [windows] fix warning C4068: unknown pragma 'clang'

--- macos/Classes/IrisMethodChannelForward.mm
+++ macos/Classes/IrisMethodChannelForward.mm
@@ -1,4 +1,10 @@
 // Relative import to be able to reuse the C sources.
 // See the comment in ../{projectName}}.podspec for more information.
+
+// Ignore warning of [-Wdocumentation] of dart_api_dl.h
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
 #include "../../src/iris_event.cc"
+#pragma clang diagnostic pop
+
 #include "../../src/iris_life_cycle_observer.mm"
