849480f4db808cbd4d46ab8ec28f5d02b6bd20dc
Gopein <z_gaopeng@sina.com>
2026-04-10 17:00:52 +0800

    test: reduce iOS integration test startup interference

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,50 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:iris_method_channel/iris_method_channel.dart';
-
 void main() {
   runApp(const MyApp());
-}
-
-class _FakePlatformBindingsDelegateInterface
-    implements PlatformBindingsDelegateInterface {
-  @override
-  int callApi(IrisMethodCall methodCall, IrisApiEngineHandle apiEnginePtr,
-      IrisApiParamHandle param) {
-    return 0;
-  }
-
-  @override
-  Future<CallApiResult> callApiAsync(IrisMethodCall methodCall,
-      IrisApiEngineHandle apiEnginePtr, IrisApiParamHandle param) async {
-    return CallApiResult(irisReturnCode: 0, data: const {});
-  }
-
-  @override
-  CreateApiEngineResult createApiEngine(List<Object> args) {
-    return const CreateApiEngineResult(IrisApiEngineHandle(0));
-  }
-
-  @override
-  IrisEventHandlerHandle createIrisEventHandler(
-      IrisCEventHandlerHandle eventHandler) {
-    return const IrisEventHandlerHandle(0);
-  }
-
-  @override
-  void destroyIrisEventHandler(IrisEventHandlerHandle handler) {}
-
-  @override
-  void destroyNativeApiEngine(IrisApiEngineHandle apiEnginePtr) {}
-
-  @override
-  void initialize() {}
-}
-
-class _FakePlatformBindingsProvider extends PlatformBindingsProvider {
-  @override
-  PlatformBindingsDelegateInterface provideNativeBindingDelegate() {
-    return _FakePlatformBindingsDelegateInterface();
-  }
 }
 
 class MyApp extends StatefulWidget {
@@ -56,20 +12,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    IrisMethodChannel irisMethodChannel =
-        IrisMethodChannel(_FakePlatformBindingsProvider());
-    await irisMethodChannel.initilize([]);
-    await irisMethodChannel.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
